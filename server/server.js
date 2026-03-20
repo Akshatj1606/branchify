3f1da9f0c52e52843e9e97d000a859797913f5e2
Adwait Patel <145041038+AdwaitPatel@users.noreply.github.com>
2026-03-20 09:43:26 +0530

    seperated app.js from server.js and added post-checkout scrpit inside hooks folder

--- server/server.js
+++ server/server.js
@@ -1,18 +1,11 @@
-import express from "express";
+import app from "./src/app.js";
 import dotenv from "dotenv";
 
 dotenv.config({
 	path: ".env.dev"
 });
 
-const app = express();
-
 const port = process.env.port || 3000;
-
-app.get("/", (req, res) => {
-	res.send("Branchify backend is running......")
-})
-
 
 app.listen(port, () => {
 	console.log(`Server is running on => http://localhost:${port}`)
