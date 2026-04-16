9c23562b7eb50d1fe320cc07995f9bf26e076dd8
Akshat <Akshatj1606@gmail.com>
2026-04-16 23:04:44 +0530

    feat: add branchify shell script to manage backend environment lifecycle

--- scripts/branchify.sh
+++ scripts/branchify.sh
@@ -1,3 +1,5 @@
+# Branchify CLI acts as a bridge between user and backend environment management system.
+
 #!/bin/bash
 
 BACKEND="http://localhost:3000"
@@ -6,11 +8,13 @@ COMMAND=$1
 BRANCH=$2
 
 case "$COMMAND" in
+# The status command fetches all environments from backend and displays them in formatted JSON.
 
     status)
         echo "Fetching environments..."
         curl -s "$BACKEND/env" | jq .
         ;;
+# First check: If branch name not given then show error
 
     delete)
         if [ -z "$BRANCH" ]; then
@@ -18,12 +22,18 @@ case "$COMMAND" in
             exit 1
         fi
 
+        # Converts:feature/login → feature_login
+
         SAFE_BRANCH=$(echo "$BRANCH" | tr '/' '_')
 
         echo "Deleting environment for branch: $BRANCH"
 
+        # Delete command removes a specific branch environment by calling backend DELETE API.
+
         curl -s -X DELETE "$BACKEND/env/$SAFE_BRANCH" | jq .
         ;;
+
+        # Stop command halts the environment without deleting it, allowing reuse later.
 
     stop)
         if [ -z "$BRANCH" ]; then
@@ -37,6 +47,8 @@ case "$COMMAND" in
 
         curl -s -X POST "$BACKEND/env/$SAFE_BRANCH/stop" | jq .
         ;;
+
+        # If user enters wrong command then this will show the usage and improve user experience.
 
     *)
         echo ""
